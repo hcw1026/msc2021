bc4e555fb91fe82b88cc9e0cf283c8bed021f51d
Chon Wai Ho (Aden) <48314854+hcw1026@users.noreply.github.com>
2021-06-14 00:05:23 +0100

    debug param initialiser

--- model.py
+++ model.py
@@ -1,9 +1,8 @@
 import tensorflow as tf
-import numpy as np
 import sonnet as snt
 
 class MetaFunClassifier(snt.Module):
-    def __init__(self,config,name="MetaFunClassifier"):
+    def __init__(self, config, name="MetaFunClassifier"):
         '''
         config: dict
             Configuation python dictionary, see ./config/sample.yaml
@@ -19,10 +18,10 @@ class MetaFunClassifier(snt.Module):
         _config = config["Model"]["comp"]
         self._use_kernel = _config["use_kernel"]
         self._use_gradient = _config["use_gradient"]
-        self._attention_type = _config["attention_type"]
-        self._kernel_type = _config["kernel_type"]
+        self._attention_type = tf.constant(_config["attention_type"],dtype=tf.string)
+        self._kernel_type = tf.constant(_config["kernel_type"],dtype=tf.string)
         self._no_decoder = _config["no_decoder"]
-        self._initial_state_type = _config["initial_state_type"]
+        self._initial_state_type = tf.constant(_config["initial_state_type"],dtype=tf.string)
 
         ## Architecture configurations
         _config = config["Model"]["arch"]
@@ -40,10 +39,10 @@ class MetaFunClassifier(snt.Module):
         self._orthogonality_penalty_weight = _config["orthogonality_penalty_weight"]
 
         # Data configurations
-        self._num_classes = config["data"]["num_classes"]
+        self._num_classes = config["Data"]["num_classes"]
 
         # Other configurations
-        self._initial_inner_lr = config["Model"]["other"]["num_classes"]
+        self._initial_inner_lr = config["Model"]["other"]["initial_inner_lr"]
         self._nonlinearity = tf.nn.relu
 
         if self._no_decoder:
@@ -58,14 +57,83 @@ class MetaFunClassifier(snt.Module):
         '''
 
         # Initialise
-        self.is_training = is_training
-        self.embedding_dim = data.tr_input.get_shape()[-1].value
+        self.is_training = tf.constant(is_training,dtype=tf.bool)
+        self.embedding_dim = data.tr_input.get_shape()[-1]
 
-        # Set data
+        # Setup data
         tr_input = data.tr_input
         val_input = data.val_input
         tr_output = data.tr_output
         val_output = data.val_output
+
+        # Initialise
+        tr_reprs = self.forward_initialiser(data.tr_input)
+        val_reprs = self.forward_initialiser(data.val_input)
+
+
+
+    def forward_initialiser(self, x):
+        num_points = tf.shape(x)[0]
+        if self._initial_state_type == tf.constant("zero",dtype=tf.string):
+            reprs = self.constant_initialiser(num_points,trainable=False)
+        elif self._initial_state_type == tf.constant("constant",dtype=tf.string):
+            reprs = self.constant_initialiser(num_points,trainable=True)
+        elif self._initial_state_type == tf.constant("parametric",dtype=tf.string):
+            reprs = self.parametric_initialiser(x)
+        else:
+            raise NameError("Unknown initial state type")
+        return reprs
+
+    def constant_initialiser(self, num_points, trainable=False):
+        if trainable:
+            init = tf.Variable(
+                tf.constant_initializer(0.)(
+                    shape=[1, self._dim_reprs],
+                    dtype = self._float_dtype),
+                    trainable=True,
+                    name="initial_state")
+
+        else:
+            init = tf.zeros([1,self._dim_reprs])
+
+        init = tf.tile(init,[num_points, self._num_classes])
+        return init
+
+    def parametric_initialiser(self, x):
+        after_dropout = tf.nn.dropout(x, rate=self._dropout_rate)
+        initializer = tf.keras.initializers.GlorotUniform() #dtype depends on input #TODO: cast input
+        module = snt.nets.MLP(
+            output_sizes=[self._nn_size] * self._nn_layers + [self._dim_reprs],
+            w_init = initializer,
+            with_bias=True,
+            activation=self._nonlinearity,
+            name="parametric_initialiser"
+        )
+        outputs = snt.BatchApply(module, num_dims=1)(after_dropout)
+        outputs = tf.tile(outputs,[1,self._num_classes])
+        return outputs
+
+
+
+if __name__ == "__main__":
+    from utils import parse_config
+    import os
+    import numpy as np
+    import collections
+    config = parse_config(os.path.join(os.path.dirname(__file__),"config/debug.yaml"))
+    module = MetaFunClassifier(config=config)
+
+    ClassificationDescription = collections.namedtuple(
+    "ClassificationDescription",
+    ["tr_input", "tr_output", "val_input", "val_output"])
+    
+    data = ClassificationDescription(
+    tf.constant(np.random.random([10,10]),dtype=tf.float32),
+    tf.constant(np.random.uniform(1,10,10).reshape(1,-1),dtype=tf.int32),
+    tf.constant(np.random.random([10,10]),dtype=tf.float32),
+    tf.constant(np.random.uniform(1,10,10).reshape(1,-1),dtype=tf.int32))
+
+    module(data)
 
         
 
