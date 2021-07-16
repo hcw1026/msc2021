143d450a5753330efcfffe9e7f7a4de57a3d48e2
Chon Wai Ho (Aden) <48314854+hcw1026@users.noreply.github.com>
2021-07-16 01:12:09 +0100

    fixed ckpt

--- utils.py
+++ utils.py
@@ -222,6 +222,7 @@ def test(testloop, model_instance, test_data, test_size, current_time=None, chec
     # Initialise Model
     if not model_instance.has_initialised:
         model_instance.initialise(next(iter(test_data)))
+        _ = model_instance(next(iter(test_data)))
 
     # Restore
     if checkpoint_path is not None:
