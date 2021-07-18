fixed logprob bug

model.py
@@ -829,6 +829,7 @@ class MetaFunRegressor(MetaFunBase, snt.Module):
             mse = self.loss_fn(mu, target_y)
             return mse
 
+
         def log_prob_loss(target_y, mus, sigmas, coeffs=None):
             mu, sigma = mus, sigmas
             dist = tfp.distributions.MultivariateNormalDiag(loc=mu, scale_diag=sigma)
@@ -838,10 +839,12 @@ class MetaFunRegressor(MetaFunBase, snt.Module):
         def loss_and_metric(loss_fn, target_y, mus, sigmas, coeffs=None):
             loss = loss_fn(target_y=target_y, mus=mus, sigmas=sigmas, coeffs=coeffs)
             mse = mse_loss(target_y=target_y, mus=mus, sigmas=sigmas, coeffs=coeffs)
-            if tf.shape(target_y)[-1] == tf.constant(0, dtype=tf.int32): # to avoid nan when computing metrics
+            tf.print("targety", target_y)
+            if tf.shape(target_y)[-1] != tf.constant(0, dtype=tf.int32): # to avoid nan when computing metrics
                 logprob = - tf.math.reduce_mean(log_prob_loss(target_y=target_y, mus=mus, sigmas=sigmas, coeffs=coeffs), axis=-1, keepdims=True)
+                tf.print("here", logprob)
             else:
-                logprob = target_y
+                logprob = target_y #empty shape
             return loss, [mse, logprob]
 
         if self._loss_type == "mse":